2618fa5107effc2dfb0d10cd57038c0e1b94a515
Nitesh <niti2002sh@gmail.com>
2025-03-29 15:29:13 +0530

    feat: Colorize terminal

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,28 +4,44 @@ import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/win
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
 import path from 'path'
+import { red, blue, yellow, green, magenta } from 'colorette'
 import * as sourceMapSupport from 'source-map-support'
 
 // Linking Trace Support
 sourceMapSupport.install()
 
-const consoleLogFormat = format.printf((info) => {
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level)
+        case 'INFO':
+            return blue(level)
+        case 'WARN':
+            return yellow(level)
+        default:
+            return level
+    }
+}
 
+const consoleLogFormat = format.printf((info) => {
+     
     const { level, message, timestamp, meta = {} } = info
 
-    const customLevel = level.toUpperCase()
+    const customLevel = colorizeLevel(level.toUpperCase())
+     
+    const customTimestamp = green(timestamp as string)
 
-    const customTimestamp = timestamp
-
+     
     const customMessage = message
 
     const customMeta = util.inspect(meta, {
         showHidden: false,
-        depth: null
+        depth: null,
+        colors: true
     })
 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${'META'} ${customMeta}\n`
+    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${magenta('META')} ${customMeta}\n`
 
     return customLog
 })
@@ -44,11 +60,12 @@ const consoleTransport = (): Array<ConsoleTransportInstance> => {
 }
 
 const fileLogFormat = format.printf((info) => {
-
+     
     const { level, message, timestamp, meta = {} } = info
 
     const logMeta: Record<string, unknown> = {}
 
+     
     for (const [key, value] of Object.entries(meta as { [key: string]: unknown })) {
         if (value instanceof Error) {
             logMeta[key] = {
@@ -59,13 +76,13 @@ const fileLogFormat = format.printf((info) => {
         } else {
             logMeta[key] = value
         }
-    }
+    }    
 
     const logData = {
         level: level.toUpperCase(),
-
+         
         message,
-
+         
         timestamp,
         meta: logMeta
     }
