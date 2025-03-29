341687d774890e849c455d32a5afc5385e8d21c3
Nitesh <niti2002sh@gmail.com>
2025-03-30 01:13:49 +0530

    feat: Health Endpoint setup

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -2,12 +2,29 @@ import { NextFunction, Request, Response } from 'express';
 import httpResponse from '../util/httpResponse';
 import responseMessage from '../constant/responseMessage';
 import httpError from '../util/httpError';
+import quicker from '../util/quicker';
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
         try {
             // throw new Error('This is error');
             httpResponse(req, res, 200, responseMessage.SUCCESS)
+        } catch (error) {
+            httpError(next, error, req, 500)
+        }    
+    },
+
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            // throw new Error('This is error');  // If i want to throw error from catch then i can use throw
+
+            const healthData = {
+                application: quicker.getApplicationHealth(),
+                system: quicker.getSystemHealth(),
+                timestamp: Date.now()
+            }
+
+            httpResponse(req, res, 200, responseMessage.SUCCESS, healthData)
         } catch (error) {
             httpError(next, error, req, 500)
         }    
--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 // router.route('self').get(apiController.self);
 router.get('/self', apiController.self);
+router.get('/health', apiController.health);
 
 export default router;
